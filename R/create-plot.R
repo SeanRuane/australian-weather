025071a937e7a269b8d4250ec936386a9813ee12
SeanRuane <Sean.Ruane@uk-cpi.com>
2025-10-27 10:52:14 +0000

    Changed title to Brisbane

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,6 +16,7 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(color = "red")
+  labs(title = "Brisbane")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
