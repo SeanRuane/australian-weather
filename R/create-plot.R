29c946fbcf72bbb50513fcdcade594bfaa95d827
SeanRuane <Sean.Ruane@uk-cpi.com>
2025-10-28 15:34:19 +0000

    time_series

--- R/create-plot.R
+++ R/create-plot.R
@@ -21,5 +21,16 @@ df %>%
   labs(title = "Brisbane")
   theme_minimal()
   facet_wrap(~WindDir9am)
+  
+  time_series =
+    df %>%
+    filter(Location == city) %>%
+    ggplot(aes(x = Date, y = Temp9am)) +
+    geom_line()
+  ggsave(filename = "graphics/timeseries-rainfall.png",
+         plot = time_series)
+  
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
+
