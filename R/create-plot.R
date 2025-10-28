9f1ffb53f9d74e978716ae239419ae3db5d1668c
SeanRuane <Sean.Ruane@uk-cpi.com>
2025-10-28 13:30:35 +0000

    comment

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,6 +16,7 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(color = "red")
+####nwq
   labs(title = "Brisbane")
   theme_minimal()
   facet_wrap(~WindDir9am)
